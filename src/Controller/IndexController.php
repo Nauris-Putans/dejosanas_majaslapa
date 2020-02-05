2026dbca9296b49f7fd72876501ba45ba6725fbc
Nauris <nauris1122@gmail.com>
2020-02-05 20:13:09 +0200

    Before push to github.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/")
      */
     public function index()
     {
-        return $this->render('index/index.html.twig', [
+        return $this->render('sections/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    /**
+     * @Route("/history")
+     */
+    public function history()
+    {
+        return $this->render('sections/dance_history.html.twig', [
             'controller_name' => 'IndexController',
         ]);
     }
